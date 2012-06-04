f6a460d17ee256dd60de2cd23343cb1a6ef6142c
Yosuke Fukuda <kesuuyof@gmail.com>
2012-06-04 23:35:26 +0900

    add windows.el and .gitignore

---
+++ .emacs.d/conf/31-windows.el
@@ -0,0 +1,10 @@
+(when (eq system-type 'windows-nt)
+  ;; font
+  (set-face-attribute 'default nil
+                      :family "Ricty Regular"
+                      :height 140)
+  (set-fontset-font "fontset-default"
+                    'japanese-jisx0208
+                    '("Ricty Regular" . "jisx0208-sjis"))
+  (add-to-list 'face-font-rescale-alist
+               `(,(encode-coding-string ".*Ricty Regular.*" 'emacs-mule) . 1.4)))
